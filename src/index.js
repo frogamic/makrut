bd6e3bb5ca9c0afacc2e3065c11087f600e50942
Dominic Shelton <frogamic@gmail.com>
2020-08-03 21:37:28 +1000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const levels = Object.fromEntries(
+  ['trace', 'debug', 'info', 'warn', 'error'].map((level, number) => [
+    level,
+    number,
+  ]),
+);
+
+const noop = () => {};
+
+const makrut = (minLevel) => {
+  const logger = Object.fromEntries(
+    Object.keys(levels).map((levelName, levelNumber) => [
+      levelName,
+      levelNumber >= levels[minLevel.toLowerCase()]
+        ? (message, object) => {
+          console[levelName](`${levelName.toUpperCase()}: ${message || ''}`);
+          if (object !== undefined) {
+            console.dir(object);
+          }
+        }
+        : noop,
+    ]),
+  );
+
+  Object.keys(logger).forEach((level) => {
+    const inspector = (message) => (object) => {
+      logger[level](message, object);
+      return object;
+    };
+
+    logger[level].inspect = (messageOrObject) => (typeof messageOrObject === 'string'
+      ? inspector(messageOrObject)
+      : inspector('')(messageOrObject)
+    );
+  });
+
+  return logger;
+};
+
+export default makrut;
